70241257fda151baa063f9e00db2899313e9d77f
Zheng Qin <kevin.qin.cn@gmail.com>
2013-07-15 12:48:52 -0400

    Added templates for time-app

--- homeMade/homeMade/views.py
+++ homeMade/homeMade/views.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponse, Http404
+from django.template import Template, Context
 import datetime
 
 def hello(request):
 	return HttpResponse("Hello world")
 
 def current_datetime(request):
+	# now = datetime.datetime.now()
+	# html = "<html><body>It is now %s.</body></html>" % now 
+	# return HttpResponse(html)
 	now = datetime.datetime.now()
-	html = "<html><body>It is now %s.</body></html>" % now 
+	# t = Template("<html><body>It is now {{ current_date }}.</body></html>") 
+	fp = open('/Users/zhengqin/Documents/WorkSpace/WebSite/Dev/HomeMadeFoodSharing/homeMade/templates/time.html')
+	t = Template(fp.read())
+	fp.close()
+	html = t.render(Context({'current_date': now}))
 	return HttpResponse(html)
 
 def hours_ahead(request, offset): 
