44cc8175f4c2bf94d54e05c96b43193e501984dd
Zheng Qin <kevin.qin.cn@gmail.com>
2013-07-15 18:39:16 -0400

    connected database

--- homeMade/homeMade/settings.py
+++ homeMade/homeMade/settings.py
@@ -11,13 +11,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'HM',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'postgres',
+        'PASSWORD': 'QZhnlg531',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
@@ -107,10 +107,13 @@ ROOT_URLCONF = 'homeMade.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'homeMade.wsgi.application'
 
+import os.path #For template_folder
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    # '/Users/zhengqin/Documents/WorkSpace/WebSite/Dev/HomeMadeFoodSharing/homeMade/templates',
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
--- homeMade/homeMade/views.py
+++ homeMade/homeMade/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse, Http404
-from django.template import Template, Context
+# from django.template import Template, Context
+# from django.template.loader import get_template 
+# from django.template import Context
+from django.shortcuts import render_to_response
 import datetime
 
 def hello(request):
@@ -9,13 +12,20 @@ def current_datetime(request):
 	# now = datetime.datetime.now()
 	# html = "<html><body>It is now %s.</body></html>" % now 
 	# return HttpResponse(html)
+	
 	now = datetime.datetime.now()
+	
 	# t = Template("<html><body>It is now {{ current_date }}.</body></html>") 
-	fp = open('/Users/zhengqin/Documents/WorkSpace/WebSite/Dev/HomeMadeFoodSharing/homeMade/templates/time.html')
-	t = Template(fp.read())
-	fp.close()
-	html = t.render(Context({'current_date': now}))
-	return HttpResponse(html)
+	
+	# fp = open('/Users/zhengqin/Documents/WorkSpace/WebSite/Dev/HomeMadeFoodSharing/homeMade/templates/time.html')
+	# t = Template(fp.read())
+	# fp.close()
+
+	# t = get_template('time.html')
+	# html = t.render(Context({'current_date': now}))
+	# return HttpResponse(html)
+
+	return render_to_response('time.html', {'current_date': now})
 
 def hours_ahead(request, offset): 
 	try:
@@ -23,5 +33,6 @@ def hours_ahead(request, offset):
 	except ValueError:
 		raise Http404()
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt) 
-	return HttpResponse(html)
+	# html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt) 
+	# return HttpResponse(html)
+	return render_to_response('hours_ahead.html', {'hour_offset':offset,'next_time':dt})
