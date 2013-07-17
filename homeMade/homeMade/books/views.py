c12ad6e218007dc0f00b8b1d1c94fd9dea467f24
Zheng Qin <kevin.qin.cn@gmail.com>
2013-07-16 20:49:51 -0400

    Added search results page

--- homeMade/homeMade/books/views.py
+++ homeMade/homeMade/books/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from homeMade.books.models import Book
 
 def search_form(request):
 	return render_to_response('search_form.html')
 
+# def search(request):
+# 	if 'q' in request.GET:
+# 		message = 'You searched for: %r' % request.GET['q'] 
+# 	else:
+# 		message = 'You submitted an empty form.' 
+# 	return HttpResponse(message)
 
 def search(request):
-	if 'q' in request.GET:
-		message = 'You searched for: %r' % request.GET['q'] 
+	if 'q' in request.GET and request.GET['q']:
+		q = request.GET['q']
+		books = Book.objects.filter(title__icontains=q) 
+		return render_to_response('search_results.html',{'books': books, 'query': q})
 	else:
-		message = 'You submitted an empty form.' 
-	return HttpResponse(message)
+		# return HttpResponse('Please submit a search term.')
+		return render_to_response('search_form.html', {'error': True})
 
 def display_meta(request):
 	values = request.META.items() 
