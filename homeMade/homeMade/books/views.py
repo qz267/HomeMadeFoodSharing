585862601c386456a22e4bfc41ac307a43f65abc
Zheng Qin <kevin.qin.cn@gmail.com>
2013-07-16 21:53:21 -0400

    Modified search_form.html

--- homeMade/homeMade/books/views.py
+++ homeMade/homeMade/books/views.py
@@ -12,14 +12,27 @@ def search_form(request):
 # 		message = 'You submitted an empty form.' 
 # 	return HttpResponse(message)
 
-def search(request):
-	if 'q' in request.GET and request.GET['q']:
-		q = request.GET['q']
-		books = Book.objects.filter(title__icontains=q) 
-		return render_to_response('search_results.html',{'books': books, 'query': q})
-	else:
-		# return HttpResponse('Please submit a search term.')
-		return render_to_response('search_form.html', {'error': True})
+# def search(request):
+# 	if 'q' in request.GET and request.GET['q']:
+# 		q = request.GET['q']
+# 		books = Book.objects.filter(title__icontains=q) 
+# 		return render_to_response('search_results.html',{'books': books, 'query': q})
+# 	else:
+# 		# return HttpResponse('Please submit a search term.')
+# 		return render_to_response('search_form.html', {'error': True})
+
+def search(request): 
+	errors = []
+	if 'q' in request.GET:
+		q = request.GET['q'] 
+		if not q:
+			errors.append('Enter a search term.')
+		elif len(q) > 20:
+			errors.append('Please enter at most 20 characters.')
+		else:
+			books = Book.objects.filter(title__icontains=q) 
+			return render_to_response('search_results.html',{'books': books, 'query': q}) 
+	return render_to_response('search_form.html',{'errors': errors})
 
 def display_meta(request):
 	values = request.META.items() 
