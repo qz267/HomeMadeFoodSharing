99ad6af40207792f2144701fb687aea6fbcb595b
Zheng Qin <kevin.qin.cn@gmail.com>
2013-07-21 11:57:15 -0400

    Added polls app

--- homeMade/books/admin.py
+++ homeMade/books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from homeMade.books.models import Publisher, Author, Book
+from books.models import Publisher, Author, Book
 
 
 class AuthorAdmin(admin.ModelAdmin):
@@ -11,7 +11,7 @@ class BookAdmin(admin.ModelAdmin):
 	list_filter = ('publication_date',)
 	date_hierarchy = 'publication_date'
 	ordering = ('-publication_date',)
-	# fields = ('title', 'authors', 'publisher')
+	fields = ('title', 'authors', 'publisher')
 	filter_horizontal = ('authors',)
 	raw_id_fields = ('publisher',)
 
